af8f5a308d103eeffd2f18b493424d3ecdc63567
Alex Halbeisen <147077701+aldentaye@users.noreply.github.com>
2023-10-05 13:15:41 -0400

    initial python flask struc

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+# Create a Flask app instance
+app = Flask(__name__)
+
+# Define a route for the home page
+@app.route('/')
+def home():
+    return 'Hello, World! This is a basic Flask app.'
+
+# Define another route
+@app.route('/about')
+def about():
+    return 'This is the about page.'
+
+# Run the Flask app if this script is executed
+if __name__ == '__main__':
+    app.run(debug=True)
